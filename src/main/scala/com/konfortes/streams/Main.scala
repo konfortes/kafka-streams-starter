83fe9cda9c64f288077c00c349ab2d129785423a
konfortes <ronen.konfortes@riskified.com>
2021-01-15 15:33:13 +0200

    some logging changes

--- src/main/scala/com/konfortes/streams/Main.scala
+++ src/main/scala/com/konfortes/streams/Main.scala
@@ -15,27 +15,29 @@ import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala.Serdes._
 import com.goyeau.kafka.streams.circe.CirceSerdes._
 import io.circe.generic.auto._
+import org.apache.kafka.streams.scala.kstream.Repartitioned
+import org.apache.kafka.streams.processor.StreamPartitioner
+import org.apache.kafka.clients.producer.internals.DefaultPartitioner
 
 object Main extends App {
   val Logger = LoggerFactory.getLogger(getClass.getSimpleName)
   Logger.info("Starting...")
 
   val conf = pureconfig.loadConfigOrThrow[AppConfig]("streams-starter")
-  println(conf)
 
   val builder = new StreamsBuilder
 
   val stream = builder
     .stream[String, User](conf.topology.sourceTopic)
-    .peek((k, v) => println(s"key: $k value: $v"))
     .filter((_, user) => user.gender == "MALE")
     .mapValues(user => user.copy(regionid = user.regionid.split("_")(1)))
+    .peek((k, v) => println(s"key: $k value: $v"))
     .to(
       conf.topology.sinkTopic
     )
 
   val topology = builder.build
-  Logger.info(topology.describe.toString)
+  Logger.debug(topology.describe.toString)
 
   val streams = new KafkaStreams(topology, conf.kafkaStreams.toProps)
 
